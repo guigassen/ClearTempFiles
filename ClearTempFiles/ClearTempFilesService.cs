349973366a414ebd12b5861c6c268531fa37f95c
Guilherme Augusto Gassen <guigassen@hotmail.com>
2020-11-02 12:31:17 -0300

    Add project files.

---
+++ ClearTempFiles/ClearTempFilesService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+
+namespace ClearTempFiles
+{
+    public class ClearTempFilesService
+    {
+
+        public void Start()
+        {
+            var _timer = new System.Timers.Timer();
+            _timer.Interval = 60000;
+            _timer.Enabled = true;
+            _timer.Elapsed += _timer_Elapsed;
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var directory = Directory.CreateDirectory(@"C:/Users/ggassen/AppData/Local/Temp");
+            foreach (FileInfo file in directory.EnumerateFiles())
+            {
+                try
+                {
+                    file.Delete();
+                }
+
+                catch (System.Exception excpt)
+                {
+                    Console.WriteLine(excpt.Message);
+                    continue;
+                }
+            }
+                 
+            foreach (DirectoryInfo dir in directory.EnumerateDirectories())
+            {
+                try
+                {
+                    dir.Delete(true);
+                }
+                catch (System.Exception excpt)
+                {
+                    Console.WriteLine(excpt.Message);
+                    continue;
+                }
+            }
+            
+            Console.WriteLine("The service is running at {0}", DateTime.Now);
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("The service has been stopped.");
+        }
+    }
+}
---
+++ ClearTempFiles/ConfigureServicecs.cs
@@ -0,0 +1,25 @@
+﻿using Topshelf;
+
+namespace ClearTempFiles
+{
+    public class ConfigureService
+    {
+        internal static void Configure()
+        {
+            HostFactory.Run(configure =>
+            {
+                configure.Service<ClearTempFilesService>(service =>
+                {
+                    service.ConstructUsing(s => new ClearTempFilesService());
+                    service.WhenStarted(s => s.Start());
+                    service.WhenStopped(s => s.Stop());
+                });
+
+                configure.RunAsLocalSystem();
+                configure.SetServiceName("ClearTempFilesService");
+                configure.SetDisplayName("ClearTempFilesService");
+                configure.SetDescription("This service will clear temp files in Windows.");
+            });
+        }
+    }
+}
